b1dbfc5bf9ffe70ed9de37fb4e8f7ba590edc09a
Janek Janiak <janek.janiak@gmail.com>
2020-10-06 23:53:20 +0200

    Adds all functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const list = document.querySelector('[data-list]');
 const input = document.querySelector('[data-input]');
 const addButton = document.querySelector('[data-addButton]');
+
+
 const items = [
     {
         value: "first item",
@@ -14,12 +16,32 @@ const items = [
 
 const addItem = () => {
     //push item to items array
+    const newItem = input.value;
+    if(newItem==='') return;
+    let item ={
+        value:newItem,
+        id:items.length+1,
+    }
+    items.push(item);
+    renderItems();
+    input.value='';
 }
 
-const removeItem = (itemId) => {
+const removeItem = (e) => {
     //filter items
+    e.target.parentNode.remove();
 }
 
 const renderItems = () => {
     //prints list
-}
+    list.textContent='';
+    items.forEach((item,index) => {
+        const listItem=document.createElement('li');
+        listItem.innerHTML=items[index].value + '<button data-deleteButton>Delete</button>';
+        list.appendChild(listItem); 
+        listItem.querySelector('[data-deleteButton]').addEventListener('click', removeItem);
+        })
+    }
+
+renderItems();
+addButton.addEventListener('click', addItem);
