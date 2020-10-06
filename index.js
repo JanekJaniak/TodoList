a69046e571174a94a2f6359033feb9a84303ef59
JanekJ <69921860+JanekJaniak@users.noreply.github.com>
2020-10-06 20:43:52 +0200

    Adds new variables and functions in js (#3)

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const list = document.querySelector('[data-list]')
-const input = document.querySelector('[data-input]')
-const addButton = document.querySelector('[data-addButton]')
+const list = document.querySelector('[data-list]');
+const input = document.querySelector('[data-input]');
+const addButton = document.querySelector('[data-addButton]');
+const items = [
+    {
+        value: "first item",
+        id: 1
+    },
+    {
+        value: "second item",
+        id: 2
+    },
+]
+
+const addItem = () => {
+    //push item to items array
+}
+
+const removeItem = (itemId) => {
+    //filter items
+}
+
+const renderItems = () => {
+    //prints list
+}
