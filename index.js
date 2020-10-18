831166e00b81102451fc2d420039dd4b10b53c10
JanekJ <69921860+JanekJaniak@users.noreply.github.com>
2020-10-18 21:54:45 +0200

    Adds all functions (#4)
    * Adds all functions
    
    * New render function
    
    * Add new render function and delete button hadler
    
    * Fished app

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const list = document.querySelector('[data-list]');
 const input = document.querySelector('[data-input]');
 const addButton = document.querySelector('[data-addButton]');
-const items = [
+
+
+let items = [
     {
         value: "first item",
         id: 1
@@ -10,16 +12,54 @@ const items = [
         value: "second item",
         id: 2
     },
+    {
+        value: "third item",
+        id: 3
+    }
 ]
 
 const addItem = () => {
-    //push item to items array
+    let item = {
+        value: input.value,
+        id: Date.now(),
+    }
+    items.push(item);
+}
+
+const handleAddButton = () => {
+    if(input.value === '') return;
+    addItem();
+    renderItems();
+    input.value = '';
 }
 
 const removeItem = (itemId) => {
-    //filter items
+    items = items.filter((item) => {
+       return item.id != itemId;
+    })   
+}
+
+const handleDeleteButton = (itemId) => {
+    removeItem(itemId);
+    renderItems();
 }
 
 const renderItems = () => {
-    //prints list
+    list.innerHTML='';
+    items.forEach((item) => {
+        const listItem = document.createElement('li');
+        const textNode = document.createTextNode(item.value);
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = 'DELETE';
+        deleteButton.setAttribute('data-item-id', item.id);
+        deleteButton.addEventListener('click', (event) => {
+            handleDeleteButton(event.target.dataset.itemId)
+        });
+        listItem.append(textNode, deleteButton);
+        list.appendChild(listItem);
+    })
 }
+
+renderItems();
+addButton.addEventListener('click', handleAddButton);
+
